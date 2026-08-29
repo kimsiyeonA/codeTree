62fdd28cde006f14a7176a33a04eb6b668b62674
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-08-29 14:07:34 +0900

    [Passed] 삼항연산자 (132ms, 10MB)

---
+++ trail1/삼항연산자/ternary-operator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       int score = sc.nextInt();
+
+       String pass = score == 100 ? "pass" : "failure";
+
+       System.out.println(pass); 
+    }
+}
