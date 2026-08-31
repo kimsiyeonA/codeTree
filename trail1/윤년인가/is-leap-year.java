062e61bda97a3c0e9af145313bebe42a82a09985
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-08-31 23:10:30 +0900

    [Passed] 윤년인가 (139ms, 10MB)

--- trail1/윤년인가/is-leap-year.java
+++ trail1/윤년인가/is-leap-year.java
@@ -6,15 +6,20 @@ public class Main {
 
         int y = sc.nextInt();
 
-        if (y % 4 == 0){
-            if(y % 100 == 0 && y % 400 != 0){
-                System.out.println("false");
-            }else{
-                System.out.println("true");
-            }
-        }else {
-            System.out.println("false");
+        // if (y % 4 == 0){
+        //     if(y % 100 == 0 && y % 400 != 0){
+        //         System.out.println("false");
+        //     }else{
+        //         System.out.println("true");
+        //     }
+        // }else {
+        //     System.out.println("false");
+        // }
 
+        if (y % 4 != 0 || (y % 100 == 0 && y % 400 != 0)){
+            System.out.println("false");
+        }else {
+            System.out.println("true");
         }
     }
 }
