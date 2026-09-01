fc384a5c16d05ae7ae97400548771a6d7b278fa3
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-02 00:39:28 +0900

    [Passed] 몫 출력하기 (131ms, 10MB)

---
+++ trail1/몫 출력하기/print-share.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int cnt = 0;
+
+        while(true){
+            int num = sc.nextInt();
+
+            if(num % 2 == 0){
+                System.out.println(num/2);
+                cnt++;
+                if(cnt == 3) break;
+            }
+        }
+    }
+}
