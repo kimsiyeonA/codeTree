afd35525c913b9ccc5d033510ad889092966ed68
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-02 00:30:25 +0900

    [Passed] 특정 수 맞히기 (125ms, 10MB)

---
+++ trail1/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int num = sc.nextInt();
+
+            if(num == 25){
+                System.out.println("Good");
+                break;
+            } else if (num > 25) {
+                System.out.println("Lower");
+            } else {
+                System.out.println("Higher");
+            }
+        }
+    }
+}
