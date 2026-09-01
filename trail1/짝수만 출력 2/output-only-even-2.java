98df375460e58643d9bb17541b47fa365efed8fd
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-01 15:53:50 +0900

    [Passed] 짝수만 출력 2 (128ms, 10MB)

---
+++ trail1/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       int b = sc.nextInt();
+       int a = sc.nextInt();
+
+       if(b % 2 != 0) b += 1;
+
+       while (b >= a){
+            System.out.print(b + " ");
+            b -= 2;
+       }
+    }
+}
