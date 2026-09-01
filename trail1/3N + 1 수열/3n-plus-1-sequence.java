435fb2bb53307f068c85ee157bb5b8b197babad2
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-02 00:58:49 +0900

    [Passed] 3N + 1 수열 (135ms, 10MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while(true){
+
+            if(n == 1){
+                break;
+            }
+
+            cnt++;
+            if(n % 2 == 0){
+                n /= 2;
+            } else{
+                n = (n * 3) + 1;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
