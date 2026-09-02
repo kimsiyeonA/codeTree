17a8269483ad02ffcc3debaccd33048dfb779814
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-02 22:42:33 +0900

    [Passed] 별표 출력하기 (139ms, 10MB)

--- trail1/별표 출력하기/print-star.java
+++ trail1/별표 출력하기/print-star.java
@@ -12,7 +12,7 @@ public class Main {
             }
             System.out.println();
         }
-        for(int i = n-1; i >= 0; i--){
+        for(int i = n-1; i > 0; i--){
             for(int j = 0; j < i; j++){
                 System.out.print("* ");
             }
