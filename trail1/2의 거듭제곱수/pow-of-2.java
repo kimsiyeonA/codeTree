d2903d565b4fbd22dd4b8f570fb756b81a3b400d
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-02 11:40:29 +0900

    [Passed] 2의 거듭제곱수 (134ms, 10MB)

---
+++ trail1/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 0;
+
+        while(true){
+            n = n/2;
+            x++;
+            if(n == 1){
+                break;
+            }
+        }
+
+        System.out.println(x);
+
+    }
+}
