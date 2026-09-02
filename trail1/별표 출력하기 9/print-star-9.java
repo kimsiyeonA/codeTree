300460e9fadea8542747dbd56dbcc5130d003e05
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-03 00:46:04 +0900

    [Passed] 별표 출력하기 9 (139ms, 10MB)

---
+++ trail1/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j =n-i-1;j >= 0; j--){
+                //System.out.println(n-i-1);
+                System.out.print("  ");
+            }
+            for(int j =1 ;j <= i + (i-1); j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
