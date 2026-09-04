af254aa374debc2860ed9013a921453fb8144718
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-04 23:37:56 +0900

    [Passed] 알파벳 출력하기 2 (134ms, 10MB)

---
+++ trail1/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 65;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i ; j++){
+                System.out.print("  ");
+            }
+            for(int j = n - i; j > 0; j--){
+                System.out.print((char)x++ + " ");
+                if(x == 91) x = 65;
+            }
+            System.out.println();
+        }
+    }
+}
