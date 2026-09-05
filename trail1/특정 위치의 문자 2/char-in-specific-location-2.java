207e148eef97fc6eb82cb1d72615ffe3e86f454c
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-05 17:04:22 +0900

    [Passed] 특정 위치의 문자 2 (133ms, 10MB)

---
+++ trail1/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] c = new char[10];
+
+        for(int i = 0; i < 10; i++){
+            c[i] = sc.next().charAt(0);
+        }
+
+        System.out.println(c[1] + " " + c[4] + " " + c[7]);
+    }
+}
