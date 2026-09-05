d08bd750975ad51468b7764a985a095d3eb4d4f5
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-06 02:06:40 +0900

    [Passed] 개수 세기 (133ms, 10MB)

---
+++ trail1/개수 세기/count-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            if(m == arr[i]) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
