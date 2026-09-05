06ece39426e359129544c0f24bab946f36dd723f
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-05 11:37:13 +0900

    [Passed] 배열 채우기 (128ms, 11MB)

---
+++ trail1/배열 채우기/filling-array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[10];
+        int cnt = 0;
+
+        for(int i = 0; i < 10; i++){
+            int num = sc.nextInt();
+            if(num == 0) break;
+            arr[i] = num;
+            cnt++;
+        }
+
+        for(int i = cnt-1; i >= 0; i--){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
