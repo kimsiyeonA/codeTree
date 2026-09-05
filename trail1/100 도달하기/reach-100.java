5b97a240acc6204e31bfaa6d5a685e48a008c5a9
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-05 22:05:04 +0900

    [Passed] 100 도달하기 (129ms, 10MB)

---
+++ trail1/100 도달하기/reach-100.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+        int cnt = 2;
+
+        arr[0] = 1;
+        arr[1] = sc.nextInt();
+
+        for(int i = 2; i < 100; i++){
+            arr[i] = arr[i -1] + arr[i - 2];
+            cnt++;
+            if(arr[i] >= 100) break;
+        }
+
+        for(int i = 0; i < cnt; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
