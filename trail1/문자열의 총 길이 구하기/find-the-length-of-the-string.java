b7583ad50f3e737a2afe62039d65e509b1aa41f9
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-07 16:59:06 +0900

    [Passed] 문자열의 총 길이 구하기 (126ms, 10MB)

---
+++ trail1/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] str = new String[10];
+        String sentens = "";
+
+        for(int i = 0; i < 10; i++){
+            str[i] = sc.next();
+        }
+
+        for(int i = 0; i < 10; i++){
+            sentens += str[i];
+        }
+
+        System.out.println(sentens.length());
+    }
+}
