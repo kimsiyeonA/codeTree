91100254daa147efea08efbcf2519736b2a458d8
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-08 15:22:44 +0900

    [Passed] 아스키코드의 합과 차 (132ms, 10MB)

---
+++ trail1/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        char b = sc.next().charAt(0);
+
+        int sum = (int)a + (int)b;
+        int minus = (int)a > (int)b ? (int)a - (int)b : (int)b - (int)a ;
+
+        System.out.print(sum + " " + minus);
+    }
+}
