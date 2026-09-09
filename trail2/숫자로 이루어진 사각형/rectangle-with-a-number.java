37efcfe57b5622f35a6a15e6a652fb6a5f2627ff
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-09 11:11:05 +0900

    [Passed] 숫자로 이루어진 사각형 (137ms, 10MB)

---
+++ trail2/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void print(int n){
+        int num = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(num++ + " ");
+                if(num == 10) num = 1;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        print(n);
+        
+    }
+}
