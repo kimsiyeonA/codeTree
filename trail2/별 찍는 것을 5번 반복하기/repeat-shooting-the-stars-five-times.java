1f953af0a6fe7ba0cd34cfac9a9ec7d5928a2284
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-09 10:04:28 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (91ms, 9MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void printStar(){
+        for(int i = 0; i < 10; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+    }
+}
