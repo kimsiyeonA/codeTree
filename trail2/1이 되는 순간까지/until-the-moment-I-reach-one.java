680b87ce9226d119a1d8a8f8a90e052065683e34
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-12 22:33:15 +0900

    [Passed] 1이 되는 순간까지 (133ms, 10MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static int cal(int n){
+        if(n == 1) return 0;
+
+        int cnt = 0;
+        if(n % 2 == 0){
+            n /= 2;
+        }else{
+            n /= 3;
+        }
+        cnt++;
+
+        return cnt + cal(n);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(cal(n));
+    }
+}
