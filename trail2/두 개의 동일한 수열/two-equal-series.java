a12d3a3b760640686c207b387dc2ea5b5d92a174
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-17 23:12:02 +0900

    [Passed] 두 개의 동일한 수열 (150ms, 10MB)

---
+++ trail2/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(a);
+        Arrays.sort(b);
+        boolean yn = true;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i]){
+                yn = false;
+                break;
+            }
+        }
+        if(yn)System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
