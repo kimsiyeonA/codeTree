09fa85d860dfa16fd360fe19bc6b64a91ad4b3bf
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2026-09-21 17:18:46 +0900

    [Passed] 키, 몸무게를 기준으로 정렬 (138ms, 10MB)

---
+++ trail2/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String[] name = new String[n];
+        int[] height = new int[n];
+        int[] weight = new int[n];
+        Person[] persons = new Person[n];
+
+        for (int i = 0; i < n; i++) {
+            name[i] = sc.next();
+            height[i] = sc.nextInt();
+            weight[i] = sc.nextInt();
+            persons[i] = new Person(name[i], height[i], weight[i]);
+        }
+
+        // Please write your code here.
+
+        Arrays.sort(persons);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(persons[i].name + " " + persons[i].height + " " + persons[i].weight);
+        }
+    }
+}
+
+class Person implements Comparable<Person>{
+    String name;
+    int height;
+    int weight;
+
+    public Person(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person person){
+        if(this.height == person.height)
+            return person.weight - this.weight;
+        return this.height - person.height;
+    }
+}
